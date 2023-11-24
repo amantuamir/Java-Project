26cf14635c2fe969dc156ba3f248614dd94a5d76
Amir Hosen <42311923+amir-hosen7@users.noreply.github.com>
2023-11-24 06:40:04 +0600

    Create PriorityQueue(MaxHeap).java

---
+++ Code/PriorityQueue(MaxHeap).java
@@ -0,0 +1,28 @@
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String arg[]) {
+    Scanner input = new Scanner(System.in);
+    int n = input.nextInt();
+    PriorityQueue<Integer> pq = new PriorityQueue<Integer>(Collections.reverseOrder());
+    for (int i = 1; i <= n; i++) {
+      int a = input.nextInt();
+      pq.add(a);
+    }
+    while (!pq.isEmpty()) {
+      int top = pq.peek();
+      System.out.println(top);
+      pq.remove();
+    }
+    input.close();
+  }
+  static void prln(Object anyObject) {
+      System.out.println(anyObject);
+  }
+  static void pnt(Object anyObject) {
+      System.out.print(anyObject);
+  }
+}
+
